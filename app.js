9ab871520f9753183b5f30f542c2ee467a73023a
Ksenia <ksenia.barshchyk@gmail.com>
2016-12-26 20:17:32 +0200

    Added simple pages server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var path = require('path');
 var express = require('express');
 
 var app = module.exports = exports = express();
@@ -7,8 +7,8 @@ var logger = require('morgan');
 
 //Configuration
 app.set('port', process.env.PORT || 8123);
-app.set('views','./views');
 app.set('view engine','jade');
+app.set('views', path.join(__dirname, 'views'));
 
 //Middleware
 app.use(logger('dev')); //app.use(express.logger('dev'));
--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@
 var app = require('./app');
 
 app.get('/', function(req,res){
-	res.send('Hello, world!')
+	//res.send('Hello, world!')
+	res.render('pages/index');
+	req.url = '/index.html';
+	next();
 });
+app.get('/*.html', function (req, res, next) {
+	res.render('pages/'+ req.params[0]);
+})
 
