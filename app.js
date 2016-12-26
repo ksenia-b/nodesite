b84d593c6462647bd6d7aaaf26dcfd5aa915f105
Ksenia <ksenia.barshchyk@gmail.com>
2016-12-26 14:39:12 +0200

    Add hello world app

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var http = require('http'),
+	express  = require('express');
+
+var app = express();
+app.get('/', function(req,res){
+	res.send('Hello, world!')
+});
+
+http.createServer(app).listen(8123,function () {
+	console.log('Visit http://localhost:8123 to begin yor work');
+});
